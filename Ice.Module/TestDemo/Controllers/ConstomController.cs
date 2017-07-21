d9d0cef2df020833e4ee47a5394ea49f9ea797ce
lvfuan@163.com <lvfuan@163.com>
2017-07-21 14:56:36 +0800

    init

---
+++ Ice.Module/TestDemo/Controllers/ConstomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDemo.Controllers
+{
+    public class ConstomController:Controller
+    {
+        public ActionResult Index(string dynamicRoute)
+        {
+            string file = "/Views/" + dynamicRoute + ".cshtml";
+            ViewBag.T = "Hello World";
+            return View(file);
+        }
+    }
+}
