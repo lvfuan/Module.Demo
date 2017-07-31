4466154a0ecd4bdb1c7474bff2e91440fcab7f10
lvfuan@163.com <lvfuan@163.com>
2017-07-31 14:12:19 +0800

    init

---
+++ Ice.Module/UnitTest.Show/ExtendTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extend.Demo;
+
+namespace UnitTest.Show
+{
+    [TestClass]
+    public class ExtendTest
+    {
+        /// <summary>
+        /// 测试StringExt
+        /// </summary>
+        [TestMethod]
+        public void TestStringExt()
+        {
+            var b="dfg".IsInt();
+        }
+        /// <summary>
+        /// 测试Student
+        /// </summary>
+        [TestMethod]
+        public void TestStudent()
+        {
+            Student stu = new Student(1,"jack","lvfuan@163.com");
+            var b= stu.isValidEmail();
+        }
+    }
+}
