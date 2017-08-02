2362fdf3a2d7b3af884474dc7bf0463b004b6529
lvfuan@163.com <lvfuan@163.com>
2017-08-02 16:31:23 +0800

    init

---
+++ Ice.Module/FromSql.CoreWeb/DB/Context/DBInit.cs
@@ -0,0 +1,29 @@
+﻿using FromSql.CoreWeb.DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromSql.CoreWeb.DB.Context
+{
+    public static class DBInit
+    {
+        public static void Initialize(FromSqlDbContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.People.Any())
+            {
+                return;
+            }
+            var peopleList = new List<PeopleModel>()
+            {
+                new PeopleModel() { Name = "张三" },
+                new PeopleModel() { Name = "李四" },
+                new PeopleModel() { Name = "王五" },
+                new PeopleModel() { Name = "赵六" }
+            };
+            peopleList.ForEach(x => context.Add(x));
+            context.SaveChanges();
+        }
+    }
+}
